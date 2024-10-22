60b133ff0d097930f6f15accdcf421e634460b1e
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-10-22 14:24:03 -0600

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 const inputField = document.querySelector("input");
 const namePlaceholder = document.querySelector('span#name-output')
 
-inputField.addEventListener('keyup', event => {
+inputField.addEventListener('input', event => {
     const enteredText = inputField.value.trim()
-    namePlaceholder.textContent = enteredText;
+    namePlaceholder.textContent = enteredText ? enteredText : 'Anonymous'
 })
